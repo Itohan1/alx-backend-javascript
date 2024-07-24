96197d80f04d5effbe9ef874cc8900f8bcbecbc1
Itohan <momoduitohan5@gmail.com>
2024-07-24 23:09:29 +0100

    Using await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,18 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const firstFunction = await uploadPhoto();
-  const secondFunction = await createUser();
-  const object = {
-    photo: { status: firstFunction.status, body: firstFunction.body },
-    user: { firstName: secondFunction.firstName, lastName: secondFunction.lastName },
-  };
-  return object;
+  try {
+    const firstFunction = await uploadPhoto();
+    const secondFunction = await createUser();
+    const object = {
+      photo: { status: firstFunction.status, body: firstFunction.body },
+      user: { firstName: secondFunction.firstName, lastName: secondFunction.lastName },
+    };
+    return object;
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
 }
