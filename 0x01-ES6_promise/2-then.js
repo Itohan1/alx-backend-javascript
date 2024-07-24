fb791f5482d5fafcd4722504bf93af9e05b844fb
Itohan <momoduitohan5@gmail.com>
2024-07-24 14:23:43 +0100

    Save 2-then.js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    promise
+      .then(() => {
+        console.log('Got a response from the API');
+        resolve({
+          status: 200,
+          body: 'success',
+        });
+      })
+      .catch(() => {
+        reject(new Error(''));
+      });
+  });
+}
