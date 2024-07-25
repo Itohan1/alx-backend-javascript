d127def457d4a375e544f7dc70a9bb0b674b8eb4
Itohan <momoduitohan5@gmail.com>
2024-07-25 22:03:16 +0100

    Implement a function named initializeRooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   const fontSize = [19, 20, 34];
-  return fontSize.map(size => new ClassRoom(size));
+  return fontSize.map((size) => new ClassRoom(size));
 }
