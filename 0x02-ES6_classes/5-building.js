330a7efd720f0684a39e9f4ff22eda99194bf8cb
Itohan <momoduitohan5@gmail.com>
2024-07-26 13:40:54 +0100

    Implement a class named Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,6 +3,9 @@ export default class Building {
     if (typeof sqft !== 'number') {
       throw new TypeError('Cannot instantiate abstract class Building directly');
     }
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
     this._sqft = sqft;
   }
 
@@ -10,7 +13,9 @@ export default class Building {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  static validate(subclass) {
+    if (typeof subclass.prototype.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 }
