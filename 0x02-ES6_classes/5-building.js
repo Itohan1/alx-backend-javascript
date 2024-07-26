9459395fadc8320204fbf61e4f27f21880f0c578
Itohan <momoduitohan5@gmail.com>
2024-07-26 15:19:05 +0100

    Implement a class named Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -3,6 +3,9 @@ export default class Building {
     if (typeof sqft !== 'number') {
       throw new TypeError('sqft must be a number');
     }
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
