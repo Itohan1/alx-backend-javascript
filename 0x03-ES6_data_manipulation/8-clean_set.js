99ce8fccd60db83f43270d3b68982e2643d31398
Itohan <momoduitohan5@gmail.com>
2024-07-30 21:03:51 +0100

    Correct output error

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (typeof startString !== 'string' || startString.length < 1) {
+  if (typeof startString !== 'string' || startString === '') {
     return '';
   }
 
