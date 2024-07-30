cd3040efaf80a15d3e6c2dd76176c391184e872c
Itohan <momoduitohan5@gmail.com>
2024-07-30 20:50:38 +0100

    Correct output error

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -2,10 +2,10 @@ export default function groceriesList() {
   const map = new Map();
   const items = [
     { name: 'Apples', quantity: 10 },
-    { name: 'Tomatoes', quantity: 5 },
+    { name: 'Tomatoes', quantity: 10 },
     { name: 'Pasta', quantity: 1 },
     { name: 'Rice', quantity: 1 },
-    { name: 'Banana', quantity: 7 },
+    { name: 'Banana', quantity: 5 },
   ];
   items.forEach((result) => {
     map.set(result.name, result.quantity);
