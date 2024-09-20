6805e15232570ccc44c6aa8d5c9386a931440e9e
Itohan <momoduitohan5@gmail.com>
2024-09-20 11:21:40 +0100

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -15,26 +15,22 @@ function countStudents(path) {
 
     const checkList1 = [];
     const checkList2 = [];
-    let lineNumber1 = 0;
-    let lineNumber2 = 0;
     file.on('line', (line) => {
       const result = line.split(',');
 
       if (result[3] === 'CS') {
-        lineNumber1 += 1;
         checkList1.push(result[0]);
       }
 
       if (result[3] === 'SWE') {
-        lineNumber2 += 1;
         checkList2.push(result[0]);
       }
     });
     file.on('close', () => {
-      const totalStudents = lineNumber1 + lineNumber2;
+      const totalStudents = checkList1.length + checkList2.length;
       console.log(`Number of students: ${totalStudents}`);
-      console.log(`Number of students in CS: ${lineNumber1}. List: ${checkList1.join(', ')}`);
-      console.log(`Number of students in SWE: ${lineNumber2}. List: ${checkList2.join(', ')}`);
+      console.log(`Number of students in CS: ${checkList1.length}. List: ${checkList1.join(', ')}`);
+      console.log(`Number of students in SWE: ${checkList2.length}. List: ${checkList2.join(', ')}`);
     });
   } catch (error) {
     console.error(error.message);
