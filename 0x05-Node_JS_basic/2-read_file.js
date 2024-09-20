efc4c1197b98954db974397c11655abb58f0eb5c
Itohan <momoduitohan5@gmail.com>
2024-09-20 10:59:11 +0100

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,2 +1,44 @@
-function countStudents {
+const fs = require('fs');
+const readline = require('readline');
+
+function countStudents(path) {
+  try {
+    const fileStream = fs.createReadStream(path);
+    const file = readline.createInterface({
+      input: fileStream,
+      output: process.stdout,
+      terminal: false,
+    });
+
+    const checkList1 = [];
+    const checkList2 = [];
+    let lineNumber1 = 0;
+    let lineNumber2 = 0;
+    file.on('line', (line) => {
+      const result = line.split(',');
+
+      if (result[3] === 'CS') {
+        lineNumber1 += 1;
+        checkList1.push(result[0]);
+      }
+
+      if (result[3] === 'SWE') {
+        lineNumber2 += 1;
+        checkList2.push(result[0]);
+      }
+    });
+    file.on('close', () => {
+      const totalStudents = lineNumber1 + lineNumber2;
+      console.log(`Number of students: ${totalStudents}`);
+      console.log(`Number of students in CS: ${lineNumber1}. List: ${checkList1.join(', ')}`);
+      console.log(`Number of students in CS: ${lineNumber2}. List: ${checkList2.join(', ')}`);
+    });
+    fileStream.on('error', () => {
+      console.error('Cannot load the database');
+    });
+  } catch (error) {
+    console.error('Cannot load the database');
   }
+}
+
+module.exports = countStudents;
