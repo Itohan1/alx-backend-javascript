539dd4b9d5c57c6eeffaa849f47d7e7b366c55b5
Itohan <momoduitohan5@gmail.com>
2024-09-20 11:48:40 +0100

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,39 +1,38 @@
 const fs = require('fs');
-const readline = require('readline');
 
 function countStudents(path) {
   try {
-    const fileStream = fs.createReadStream(path);
-    fileStream.on('error', () => {
-      throw new Error('Cannot load the database');
-    });
-    const file = readline.createInterface({
-      input: fileStream,
-      output: process.stdout,
-      terminal: false,
-    });
+    const fileStream = fs.readFileSync(path, 'utf-8');
+    const dlines = fileStream.trim().split('\n').filter((line) => line);
+
+    const students = dlines.slice(1);
+
+    if (students.length === 0) {
+      throw new Error('No valid students in the file.');
+    }
 
     const checkList1 = [];
     const checkList2 = [];
-    file.on('line', (line) => {
-      const result = line.split(',');
 
-      if (result[3] === 'CS') {
-        checkList1.push(result[0]);
+    students.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+
+      if (!firstname || !field) {
+        return;
       }
 
-      if (result[3] === 'SWE') {
-        checkList2.push(result[0]);
+      if (field === 'CS') {
+        checkList1.push(firstname);
+      } else if (field === 'SWE') {
+        checkList2.push(firstname);
       }
     });
-    file.on('close', () => {
-      const totalStudents = checkList1.length + checkList2.length;
-      console.log(`Number of students: ${totalStudents}`);
-      console.log(`Number of students in CS: ${checkList1.length}. List: ${checkList1.join(', ')}`);
-      console.log(`Number of students in SWE: ${checkList2.length}. List: ${checkList2.join(', ')}`);
-    });
+
+    console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students in CS: ${checkList1.length}. List: ${checkList1.join(', ')}`);
+    console.log(`Number of students in SWE: ${checkList2.length}. List: ${checkList2.join(', ')}`);
   } catch (error) {
-    console.error(error.message);
+    throw new Error('Cannot load the database');
   }
 }
 
