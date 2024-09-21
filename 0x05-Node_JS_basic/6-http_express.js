a0c3c1ef0e04c5d3411cc2a846c7a8774fc63a0a
Itohan <momoduitohan5@gmail.com>
2024-09-21 15:52:30 +0100

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
-const app = express().listen(1245, () => {
-  console.log('Hello Holberton School!');
-});
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+}).listen(1245);
 
 module.exports = app;
