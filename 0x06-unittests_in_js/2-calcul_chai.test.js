9633bc71b03faa817ec706d3f6f7598415ba5a4f
Itohan <momoduitohan5@gmail.com>
2024-09-27 20:10:43 +0100

    corrections

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,23 +1,20 @@
-(async () => {
-  const chai = await import('chai');
-  const { expect } = chai;
-  const calNum = await import('./2-calcul_chai');
+const expect = require('chai');
+const calNum = require('./2-calcul_chai');
 
-  describe('calNum', () => {
-    it('should round a and b and return the sum', () => {
-      expect(calNum('SUM', 1.4, 4.5)).to.equal(6);
-    });
-
-    it('should round a and b and return the subtraction', () => {
-      expect(calNum('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-    });
-
-    it('should round a and b and return the division', () => {
-      expect(calNum('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-    });
-
-    it('should round a and b and return the division', () => {
-      expect(calNum('DIVIDE', 1.4, 0)).to.equal('Error');
-    });
+describe('calNum', () => {
+  it('should round a and b and return the sum', () => {
+    expect(calNum('SUM', 1.4, 4.5)).to.equal(6);
   });
-})();
+
+  it('should round a and b and return the subtraction', () => {
+    expect(calNum('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+  });
+
+  it('should round a and b and return the division', () => {
+    expect(calNum('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+  });
+
+  it('should round a and b and return the division', () => {
+    expect(calNum('DIVIDE', 1.4, 0)).to.equal('Error');
+  });
+});
